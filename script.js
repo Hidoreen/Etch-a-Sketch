51b9c2be505ba659c8fae067cfa23e4f7e3c83e6
doreen <doreenwangeci38@gmail.com>
2023-11-12 23:00:43 +0300

    Added a createGrid dunction that allow users to input their own grid size

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 
 const container  = document.querySelector('.container');
+let currentGridSize = 16;
 
 //create Grid
 function getGrid(num){
+
+    container.innerHTML = ''; // Clear existing content
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    currentGridSize = num;
+
+
     for(let i = 0; i < num*num; i++){
             const square = document.createElement('div');
             square.className = 'square-grid';
             container.appendChild(square);
 
             square.addEventListener('mouseover', (e)=>{
-                e.target.style.backgroundColor = '#757575';
+                e.target.style.backgroundColor = '#6d4e00';
             })
 
         }
 }
-
-getGrid(16);
 
 //reset the grid
 function resetGrid(){
@@ -28,5 +34,29 @@ function resetGrid(){
 const resetBtn = document.querySelector('.resetbtn');
 resetBtn.addEventListener('click', resetGrid);
 
+//clear grid
+
+function clearGrid(){
+    container.innerHTML = '';
+}
+
 
 //user create own grid 
+
+function userGrid(){
+    const userGridSize = document.getElementById('gridSize');
+
+    let userGridSizeValue = userGridSize.value;
+
+
+    if(userGridSizeValue < 16 || userGridSizeValue > 100){
+        alert('Enter a number between 16 - 100');
+    } else {
+        clearGrid();
+        getGrid(userGridSizeValue);  
+    }
+}
+
+const createGrid = document.getElementById('userGrid');
+
+createGrid.addEventListener('click', userGrid);
